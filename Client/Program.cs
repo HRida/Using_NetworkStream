a9e87e1333ca404560f305a8865c74412311f4b4
HRida <H.Rida1@hotmail.com>
2020-05-23 11:51:22 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter streamWriter;
+            StreamReader streamReader;
+            NetworkStream networkStream;
+            TcpListener tcpListener = new TcpListener(3333);
+            tcpListener.Start();
+            Console.WriteLine("Server Started. Waiting for Client Connection at port 3333");
+            Socket serverSocket = tcpListener.AcceptSocket();
+            try
+            {
+                if (serverSocket.Connected)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Client Connected at port 3333");
+                        networkStream = new NetworkStream(serverSocket);
+                        streamWriter = new StreamWriter(networkStream);
+                        streamReader = new StreamReader(networkStream);
+                        Console.WriteLine(streamReader.ReadLine() + "Yasser");
+                    }
+                }
+                if (serverSocket.Connected)
+                    serverSocket.Close();
+                Console.Read();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient;
+            NetworkStream networkStream;
+            StreamReader streamReader;
+            StreamWriter streamWriter;
+            try
+            {
+                tcpClient = new TcpClient("localhost", 3333);
+                networkStream = tcpClient.GetStream();
+                streamReader = new StreamReader(networkStream);
+                streamWriter = new StreamWriter(networkStream);
+                streamWriter.WriteLine("Client connected at port 3333");
+                streamWriter.Flush();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.Read();
+        }
+    }
+}
